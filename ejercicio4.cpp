317130b7dbd38856ac6c852a4700a5cedabdd132
CesarBlvD <86888362+CesarBlvD@users.noreply.github.com>
2021-11-04 13:05:35 -0600

    divisor

---
+++ ejercicio4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib>
+#include<iostream>
+
+int main()
+{
+    float numero1=3.3;
+    float numero2=6.6;
+    float division=numero1/numero2;
+
+    printf("La division de %.2f y el numero %.2f el resultado es: %.2f",numero1,numero2,division);
+}
